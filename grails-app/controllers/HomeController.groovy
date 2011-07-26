96c469baa88524f4525d9e8e183f76db002357a8
Maciej Hryniszak <maciej.hryniszak@travelocity.com>
2011-07-26 15:08:38 +0200

    Updated person names to be more human

--- grails-app/controllers/HomeController.groovy
+++ grails-app/controllers/HomeController.groovy
@@ -2,10 +2,10 @@ class HomeController {
 	def index = {
 		Thread.start {
 			Person.withNewSession { session ->
-				new Person(firstName: "a", lastName: "b").save(flush: true)
+				new Person(firstName: "John", lastName: "Doe").save(flush: true)
 			}
 			Person.withNewSession { session ->
-				new Person(firstName: "c", lastName: "d").save(flush: true)
+				new Person(firstName: "Jane", lastName: "Smith").save(flush: true)
 			}
 		}
 		render text: "OK", contentType: "text/plain"
